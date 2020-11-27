4e120a7d9b60de6f9e094e36388835137667eebf
Thiago Moreira Bianeck <thiagobianeck@gmail.com>
2020-11-27 17:33:06 -0300

    - Criação do módulo de configuração

---
+++ server/config/env/development.env.ts
@@ -0,0 +1,12 @@
+module.exports = {
+    env: 'development',
+    db: 'ts-api',
+    dialect: 'postgres',
+    username: 'postgres',
+    password: 'developer2020',
+    host: 'localhost',
+    serverPort: 3000,
+    pgPort: 5432,
+    dbUrl: 'postgres://postgres:developer2020@localhost:5432/ts-api',
+    secret: 'S3cr3t'
+}
---
+++ server/config/env/test.env.ts
@@ -0,0 +1,12 @@
+module.exports = {
+    env: 'test',
+    db: 'ts-api-test',
+    dialect: 'postgres',
+    username: 'postgres',
+    password: 'developer2020',
+    host: 'localhost',
+    serverPort: 3000,
+    pgPort: 5432,
+    dbUrl: 'postgres://postgres:developer2020@localhost:5432/ts-api-test',
+    secret: 'S3cr3t'
+}
