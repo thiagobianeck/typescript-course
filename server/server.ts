022ea81ef1333d2f921734212703c0decaa1e357
Thiago Moreira Bianeck <thiagobianeck@gmail.com>
2020-11-27 18:19:23 -0300

    - Refatoração

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,11 @@
 import * as http from 'http';
 import Api from './api/api';
-import { errorHandlerApi } from "./api/errorHandlerApi";
+
 
 const config = require('./config/env/config')();
 
 const server = http.createServer(Api);
 
-Api.use(errorHandlerApi);
-
-server.listen(config.serverPort, () => console.log('Server está rodando na porta 3000'));
+server.listen(config.serverPort);
+server.on('listening', () => console.log(`Servidor está rodando na porta ${config.serverPort}`));
+server.on("error", (error: NodeJS.ErrnoException) => console.error(`Ocorreu um erro: ${error}`));
